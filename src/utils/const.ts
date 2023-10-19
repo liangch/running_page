8039fa959f6056a497c93e5f8a1d26015316ec76
Liang Chen <lc@liangchen.net>
2023-10-19 13:43:30 +0800

    misc

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoibGlhbmdjaDIiLCJhIjoiY2sxazR6cDBtMGFidDNncGVzM3g4dHlucyJ9.ZKcv2UGsd6VKtL4M6wwoDA';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
