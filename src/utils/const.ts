162faee68d8e57cbaf231c8e339848f7707cb38c
Liang Chen <175010+liangch@users.noreply.github.com>
2025-01-06 10:40:49 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -34,7 +34,7 @@ const LINE_OPACITY = 0.9;
 // styling: map height
 const MAP_HEIGHT = 600;
 //set to `false` if you want to hide the road label characters
-const ROAD_LABEL_DISPLAY = true;
+const ROAD_LABEL_DISPLAY = false;
 // update for now 2024/11/17 the privacy mode is true
 //set to `true` if you want to display only the routes without showing the map.
 const PRIVACY_MODE = false;
